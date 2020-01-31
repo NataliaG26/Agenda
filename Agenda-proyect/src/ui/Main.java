69becb37ace98938909692a5b2abeabd1d9c552d
Lina Salinas <linasalinasd7@gmail.com>
2020-01-31 16:27:55 -0500

    The fxml file has been added and the controller class has been created.

--- Agenda-proyect/src/ui/Main.java
+++ Agenda-proyect/src/ui/Main.java
@@ -1,9 +1,24 @@
 package ui;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Main {
 	
 	public static void main(String args[]) {
 		
+	}
+	
+	public void start(Stage stage) throws Exception {
+
+		Parent root = FXMLLoader.load(getClass().getResource("Agenda.fxml"));
+
+		Scene scene = new Scene(root);
+		stage.setTitle("Agenda");
+		stage.setScene(scene);;
+		stage.show();
 	}
 
 }
