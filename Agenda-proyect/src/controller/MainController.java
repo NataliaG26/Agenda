9fcc4b9174c9d5c043d58a3f1c8ac537b8570b5b
Natalia Gonzalez <nataliaisagonzalez26@gmail.com>
2020-02-08 20:59:25 -0500

    User interface modified
    The Agenda window had been modified, and add functionalities for a more pleasant user experience

---
+++ Agenda-proyect/src/controller/MainController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.fxml.FXMLLoader;
+import model.Agenda;
+
+public class MainController {
+	
+	private Agenda agenda;
+	
+	public MainController() {
+		agenda = new Agenda();
+		
+	}
+	
+	 public void conection(FXMLLoader fxml, String controller) {
+		 	if(controller.equals("Agenda")) {
+				AgendaController obj = fxml.getController();
+				obj.setMainController(this);
+			}else if(controller.equals("Contact")) {
+				ContactController obj = fxml.getController();
+				obj.setMainController(this);
+			}else if(controller.equals("Summary")) {
+				SummaryController obj = fxml.getController();
+				obj.setMainController(this);
+			}
+			
+		}
+	
+
+}
